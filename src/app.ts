89290f37de2ce7de5ab70286d06e7398aa73ab2a
Duc Vo <pianote@gmail.com>
2020-11-20 15:29:07 -0800

    add some initial project

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,26 @@
 import {ProjectInput} from './ProjectInput';
 import {ProjectStatus} from './Project';
 import {ProjectList} from './ProjectList';
+import {projectState} from './ProjectState';
 
 
 new ProjectInput();
-new ProjectList(ProjectStatus.ACTIVE);
-new ProjectList(ProjectStatus.FINISHED);
+const activeProjects = new ProjectList(ProjectStatus.ACTIVE);
+const finishedProjects = new ProjectList(ProjectStatus.FINISHED);
+
+
+projectState.addProject(
+	'Maskes Backend - Django',
+	'Backend Rest API using python and Django framework ',
+	2,
+	ProjectStatus.FINISHED
+);
+
+projectState.addProject(
+	'Maskes Frontend - ReactJS',
+	'Front End using React and Redux',
+	4,
+	ProjectStatus.ACTIVE
+); 
+activeProjects.renderContent();
+finishedProjects.renderContent();
